53e3b730ec7c2580661e19a996ad186b9adcdf34
tejerolucas <tejerolucas@gmail.com>
2023-11-30 20:28:52 -0300

    Create Company.dart

---
+++ lib/Models/Company.dart
@@ -0,0 +1,73 @@
+import 'package:secretweapon/Models/CompanyColors.dart';
+import 'package:secretweapon/Models/CompanyModule.dart';
+import 'package:secretweapon/Models/LoginSettings.dart';
+
+class Company {
+  String id = "";
+  String name = "";
+  String image = "";
+  CompanyColors colors = CompanyColors();
+  LoginSettings loginSettings = LoginSettings();
+  List<CompanyModule> modules = [];
+
+  //created date
+  DateTime createdDate = DateTime.now();
+
+  Map<String, String> users = {};
+  List<String> roles = [];
+
+  bool enableNewAccounts = false;
+
+  //get modules enabled
+  List<CompanyModule> get enabledModules => modules.where((element) => element.enabled).toList();
+
+  Company();
+
+  Company.fromDocument(String id, Map<String, dynamic> data) {
+    this.id = id;
+    this.name = data["name"] ?? '';
+    this.image = data["image"] ?? "";
+    this.colors = data["colors"] != null ? CompanyColors.fromDocument(data["colors"]) : CompanyColors();
+    this.loginSettings =
+        data["loginSettings"] != null ? LoginSettings.fromDocument(data["loginSettings"]) : LoginSettings();
+    this.modules = data["modules"] != null ? LoadModules(data["modules"]) : [];
+    this.createdDate = data["createdDate"] != null ? data["createdDate"].toDate() : DateTime.now();
+    this.users = data["users"] != null ? Map<String, String>.from(data["users"]) : {};
+    this.roles = data["roles"] != null ? List<String>.from(data["roles"]) : [];
+    this.enableNewAccounts = data["enableNewAccounts"] ?? true;
+  }
+
+  LoadModules(List<dynamic> data) {
+    modules.clear();
+    for (var _module in data) {
+      var module = CompanyModule.fromDocument(_module["moduleId"], _module);
+      modules.add(module);
+    }
+
+    modules.sort((a, b) => a.order.compareTo(b.order));
+    return modules;
+  }
+
+  toJson() {
+    return {
+      "name": name,
+      "image": image,
+      "colors": colors.toJson(),
+      "loginSettings": loginSettings.toJson(),
+      "users": users,
+      "roles": roles,
+      "createdDate": createdDate,
+      "enableNewAccounts": enableNewAccounts,
+      "modules": modules.map((e) {
+        return e.toJson();
+      }).toList(),
+    };
+  }
+
+  String getUserRole(String userId) {
+    if (users.containsKey(userId)) {
+      return users[userId] ?? "";
+    }
+    return "";
+  }
+}
